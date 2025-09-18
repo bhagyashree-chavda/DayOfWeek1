9e8e654707ef2eac01c250208444201c27b7c61b
bhagyashree-chavda <cbbhagyashree78@gmailcom>
2025-09-18 13:10:35 +0200

    initiate

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -1,26 +1,50 @@
 import java.util.Scanner;
 
-public class DayOfWeek {
-    public void  start()
+public class DayOfWeek
+{
+    public void start()
+    {
+        Scanner sc = new Scanner(System.in);
+        int day = 0;
+        System.out.println("Enter the day of the week beetween 1 to 7");
+        while (true)
         {
-            System.out.println("enter the day of the week");
-            Scanner sc=new Scanner(System.in);
-            int day=sc.nextInt();
-            switch(day)
-                {
-                case 1-> System.out.println("its a monday");
-                case 2-> System.out.println("its a tuesday");
-                case 3-> System.out.println("its a wednesday");
-                case 4-> System.out.println("its a thursday");
-                case 5-> System.out.println("its a friday");
-                case 6-> System.out.println("its a saturday");
-                case 7-> System.out.println("its a sunday");
-                    default ->  System.out.println("invalid day");
-
-                }
-                sc.close();
+            System.out.print("your number is ");
+            if (sc.hasNextInt())
+            {
+                day = sc.nextInt();
+                break;
+            }
+            else
+            {
+                System.out.println("enter a valid number between 1 and 7");
+                sc.next();
+            }
         }
 
+        String result = getDay(day);
 
+        System.out.println("The day of the week is: " + result);
+        sc.close();
+    }
+
+    public String getDay(int day)
+    {
+        String dayString = "";
+        switch (day) {
+            case 1 -> dayString = "Sunday";
+            case 2 -> dayString = "Monday";
+            case 3 -> dayString = "Tuesday";
+            case 4 -> dayString = "Wednesday";
+            case 5 -> dayString = "Thursday";
+            case 6 -> dayString = "Friday";
+            case 7 -> dayString = "Saturday";
+            default -> dayString = "Invalid day";
+
+        }
+        return dayString;
+    }
 
 }
+
+
